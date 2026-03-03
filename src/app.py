94bd2eef989cfb32874bdee0af7c979963fbd612
Morena <morenicora.dev@gmail.com>
2026-03-03 00:44:51 +0000

    my first commit

--- src/app.py
+++ src/app.py
@@ -2,21 +2,22 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets
+from sqlalchemy import select
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,55 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def User():
+    all_users = db.session.execute(select(User)).scalars().all()
+    results = list(map(lambda user: user.serialize(), all_users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(results), 200
 
-    return jsonify(response_body), 200
+@app.route('/characters', methods=['GET'])
+def all_characters():
+    all_characters = db.session.execute(select(characters)).scalars().all()
+    results = list(map(lambda character: character.serialize(), all_characters))
+
+    return jsonify(results), 200
+
+
+@app.route('/characters/<int:characters_id>', methods=['GET'])
+def characters():
+    characters = db.session.execute(select(Characters)).scalars().all()
+    results = list(map(lambda character: character.serialize(), characters))
+
+    return jsonify(results), 200
+
+@app.route('/planets', methods=['GET'])
+def all_planets():
+    all_planets = db.session.execute(select(Planets)).scalars().all()
+    results = list(map(lambda planet: planet.serialize(), all_planets))
+
+    return jsonify(results), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def planets():
+    characters = db.session.execute(select(Characters)).scalars().all()
+    results = list(map(lambda character: character.serialize(), characters))
+
+    return jsonify(results), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
